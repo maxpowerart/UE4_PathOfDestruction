a58c1ac11e7b7ed49c271036dcb08a86012ffa2e
maxpowerart <dart-sion1@yandex.ru>
2021-02-27 17:43:40 +0300

    Init

---
+++ Source/UE4_PoD/UE4_PoDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_PoDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_POD_API AUE4_PoDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
